c098e27ab01fc7b8f6cfd4d9ef91ba22a7df1c42
ytyaru <ytyaru@outlook.jp>
2019-09-18 07:48:09 +0900

    SQLファイル毎回初期化（前回分まで追記されるバグ修正）

--- src/insert_news.sh
+++ src/insert_news.sh
@@ -6,6 +6,8 @@ run() {
 	local json_path="$1"
 	local insert_sql="insert.sql"
 	[ 'ok' != "`json_extract "$json_path" '$.status'`" ] && { echo 'エラー。JSONのstatusがokでない。: '"`json_extract "$json_path" '$.status'`" 1>&2; exit 1; }
+	# SQLファイル内容を空にする（さもなくば連続使用時に前の分と合わせて追記されてしまう）
+	: > "$insert_sql"
 	local totalResults="`json_extract "$json_path" '$.totalResults'`"
 	for idx in $(seq 0 $(expr $totalResults - 1)); do
 		# JSONから項目を抽出する
@@ -13,6 +15,8 @@ run() {
 		local url="`json_extract "$json_path" '$.articles['"$idx"'].url'`"
 		local title="`json_extract "$json_path" '$.articles['"$idx"'].title'`"
 		local body="`json_extract "$json_path" '$.articles['"$idx"'].description'`" # とりあえずdescriptionで代用する
+		# totalResultsが多すぎたとき各項目はNULL(空文字)になる。このときは終了する。JSONが正しい限り起こり得ない。
+		[ -z "$title" ] && { echo "JSON不正。titleが空。totalResults:$totalResults,idx:$idx" 1>&2; break; }
 		# insert文を作る
 		make_insert_stmt "$published" "$url" "$title" "$body" >> "$insert_sql"
 	done
